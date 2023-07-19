96d93a6b10c47d3361523f0a206a8dd992cba389
unknown <rakkakeren@gmail.com>
2023-07-20 02:18:17 +0700

    change: save photo url to local storage

--- stores/user.js
+++ stores/user.js
@@ -3,10 +3,17 @@ export const useUserStore = defineStore("user", () => {
     // User Profile
     const photoUrl = ref("")
     function getPhotoUrl() {
+        if (process.client) {
+            console.log("client: ", localStorage.getItem("photoUrl"))
+            return localStorage.getItem("photoUrl") || photoUrl.value
+        }
         return photoUrl.value
     }
     function setPhotoUrl(url) {
         photoUrl.value = url
+        if (process.client) {
+            localStorage.setItem("photoUrl", url)
+        }
     }
 
     return {getPhotoUrl, setPhotoUrl}
