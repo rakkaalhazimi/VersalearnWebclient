e458e5d619079806f8cacbd658b53e3bdef60022
Rakka Alhazimi <rakkakeren@gmail.com>
2024-03-16 23:26:48 +0700

    add: login and register form

--- pages/login.vue
+++ pages/login.vue
@@ -16,8 +16,17 @@ const { $googleLogin } = useNuxtApp()
         <div>
           <h3>Login</h3>
         </div>
-        <div>
-          <ButtonNeutral @click="$googleLogin" class="flex items-center gap-x-4">
+        <div class="flex flex-col gap-y-8">
+          <form class="flex flex-col gap-y-2">
+            <InputText placeholder="Username"></InputText>
+            <InputPassword placeholder="Password"></InputPassword>
+            <ButtonSecondary>Login</ButtonSecondary>
+          </form>
+          <div>
+            <p>Don't have an account? <NuxtLink to="/register">register here</NuxtLink>
+            </p>
+          </div>
+          <ButtonNeutral @click="$googleLogin" class="flex items-center gap-x-4 mx-auto">
             <font-awesome-icon icon="fa-brands fa-google"></font-awesome-icon>
             <span class="flex align-middle">Login with Google</span>
           </ButtonNeutral>
---
+++ pages/register.vue
@@ -0,0 +1,28 @@
+<script setup>
+const { $googleLogin } = useNuxtApp()
+</script>
+
+
+<template>
+  <Navbar>
+    <template v-slot:left>
+      <NavbarLinkLogo link="/">LOGO</NavbarLinkLogo>
+    </template>
+  </Navbar>
+
+  <main>
+    <SectionBelowNavbar id="register">
+      <div class="flex flex-col justify-center items-center gap-y-5 p-8 mx-auto max-w-sm w-full border">
+        <h3>Register</h3>
+        <form class="flex flex-col gap-y-8">
+          <div class="flex flex-col gap-y-2">
+            <InputText placeholder="Username"></InputText>
+            <InputPassword placeholder="Password"></InputPassword>
+            <InputPassword placeholder="Verify Password"></InputPassword>
+            <ButtonSecondary>Register</ButtonSecondary>
+          </div>
+        </form>
+      </div>
+    </SectionBelowNavbar>
+  </main>
+</template>
