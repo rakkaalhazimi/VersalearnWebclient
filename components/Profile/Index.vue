770fc1933bd8b9aaf0724fe7b42e98757f0ad2f0
Rakka Alhazimi <rakkakeren@gmail.com>
2024-03-16 08:34:32 +0700

    change: remove unnecessary client-only tag

--- components/Profile/Index.vue
+++ components/Profile/Index.vue
@@ -10,29 +10,27 @@
 
 <template>
   <div class="relative self-center">
-    <client-only>
-      <img @click="openProfileCard" :src="userStore.photoUrl" class="h-8 w-8 border-1 rounded-full"/>
-      <ul
-        class="absolute top-14 right-0 p-4 flex flex-col gap-y-2 h-44 w-72 bg-gray-50 border-2 border-gray-200 text-sm"
-        :class="{'block': isProfileCardOpen, 'hidden': !isProfileCardOpen}"
+    <img @click="openProfileCard" :src="userStore.photoUrl" class="h-8 w-8 border-1 rounded-full"/>
+    <ul
+      class="absolute top-14 right-0 p-4 flex flex-col gap-y-2 h-44 w-72 bg-gray-50 border-2 border-gray-200 text-sm"
+      :class="{'block': isProfileCardOpen, 'hidden': !isProfileCardOpen}"
 
-      >
-        <li class="grid grid-cols-2">
-          <span>Name</span>
+    >
+      <li class="grid grid-cols-2">
+        <span>Name</span>
           <span>{{ userStore.name }}</span>
-        </li>
-        <li class="grid grid-cols-2">
-          <span>Packet</span>
-          <span>Free</span>
-        </li>
-        <li class="grid grid-cols-2">
-          <span>Quota</span>
-          <span>10 of 10</span>
-        </li>
-        <li class="mt-auto ml-auto">
-          <ButtonSecondary @click="$googleLogout">Logout</ButtonSecondary>
-        </li>
-      </ul>
-    </client-only>
+      </li>
+      <li class="grid grid-cols-2">
+        <span>Packet</span>
+        <span>Free</span>
+      </li>
+      <li class="grid grid-cols-2">
+        <span>Quota</span>
+        <span>10 of 10</span>
+      </li>
+      <li class="mt-auto ml-auto">
+        <ButtonSecondary @click="$googleLogout">Logout</ButtonSecondary>
+      </li>
+    </ul>
   </div>
 </template>
