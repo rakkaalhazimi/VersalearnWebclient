3ede501f9ebb4d860ed435dc4f343b5155643138
Rakka Alhazimi <rakkakeren@gmail.com>
2024-03-17 00:04:12 +0700

    add: text and password input

---
+++ components/Input/Password.vue
@@ -0,0 +1,11 @@
+<script setup>
+  let defaultPlacholder = "Password"
+  let passwordModel = defineModel()
+  defineProps({
+    placeholder: String,
+  })
+</script>
+
+<template>
+  <input type="password" v-model="passwordModel" :placeholder="placeholder || defaultPlacholder" class="border focus:outline-none py-3 px-6">
+</template>
---
+++ components/Input/Text.vue
@@ -0,0 +1,11 @@
+<script setup>
+  let defaultPlacholder = "Insert Text"
+  let textModel = defineModel()
+  defineProps({
+    placeholder: String,
+  })
+</script>
+
+<template>
+  <input type="text" v-model="textModel" :placeholder="placeholder || defaultPlacholder" class="border focus:outline-none py-3 px-6">
+</template>
